efb9b676f15cd19fde616fc476902f5c393bb14d
ZaL <z4ls@live.com>
2018-06-24 18:09:44 +0300

    multiple orders

--- src/customer/orders/OrderDetailScene.js
+++ src/customer/orders/OrderDetailScene.js
@@ -5,15 +5,13 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {SELECTORS as ORDER_SELECTORS} from 'customer/common/selectors';
-import {ScrollView, View} from 'react-native';
+import {ScrollView, Text, View} from 'react-native';
 import OrderItems from 'customer/orders/components/OrderItems';
 import OrderBasicInfo from 'customer/orders/components/OrderBasicInfo';
 import OrderTotal from 'customer/orders/components/OrderTotal';
 import {bindActionCreators} from 'redux';
 import {ACTIONS} from 'customer/common/actions';
-import SectionHeading from 'components/SectionHeading';
-import I18n from 'utils/locale';
-import QRCode from 'react-native-qrcode-svg';
+import QRCodes from "customer/orders/components/QRCodes";
 
 class OrderDetailScene extends Component {
   static propTypes = {
@@ -43,19 +41,7 @@ class OrderDetailScene extends Component {
           <OrderBasicInfo item={order} />
           <OrderItems order={order} />
           <OrderTotal total={order.total} />
-          <View>
-            <SectionHeading title={I18n.t('qr_code')} />
-            <View style={{flex: 1, padding: 20}}>
-              {order.products &&
-              order.products.map(product => {
-                return (
-                  <View style={{marginBottom: 80, alignItems: 'center'}} key={`{product.id}`}>
-                    <QRCode value={`${product.id}`}  />
-                  </View>
-                );
-              })}
-            </View>
-          </View>
+          <QRCodes order={order}/>
         </ScrollView>
       );
     }
---
+++ src/customer/orders/components/QRCodes.js
@@ -0,0 +1,50 @@
+/**
+ * @flow
+ */
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import colors from 'assets/theme/colors';
+import SectionHeading from 'components/SectionHeading';
+import I18n from 'utils/locale';
+import QRCode from 'react-native-qrcode-svg';
+
+const QRCodes = ({order}) => {
+  return (
+    <View styles={styles.container}>
+      <SectionHeading title={I18n.t('qr_code')} />
+      <View style={styles.rowContainer}>
+        {order.products &&
+        order.products.map(product => {
+          console.log('product',product);
+          return (
+            <View style={{marginBottom: 80, alignItems: 'center'}} key={`${product.id}`}>
+              <QRCode value={`${product.id}`}  />
+              <View style={{flexDirection:'row',alignItems:'center'}}>
+                <Text style={{paddingVertical:10,fontSize:17}}>{product.name}</Text>
+                {
+                  product.company &&
+                  <Text style={{paddingVertical:10,fontSize:17}}> - {product.company.name}</Text>
+                }
+              </View>
+            </View>
+          );
+        })}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    padding:10,
+  },
+  total: {
+    color: colors.primary,
+    fontSize: 22,
+    textAlign: 'right',
+  },
+  rowContainer:{flex: 1, padding: 20}
+});
+
+export default QRCodes;
